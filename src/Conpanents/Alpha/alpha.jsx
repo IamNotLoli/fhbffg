4669dae81e4581520c75132913a00e31d00a26e9
IamNotLoli <101261570+IamNotLoli@users.noreply.github.com>
2022-03-14 17:03:23 +0500

    Add files via upload

---
+++ src/Conpanents/Alpha/alpha.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import "../../App.css"
+import { Beta } from "../Beta/beta";
+
+
+export const Alpha = ()=>{
+    return(
+        <div>
+            <section className="ra1">
+                <div>
+        
+                </div>
+            </section>
+            <Beta />
+        </div>
+    )
+}
